c6b40ab535cecf5076dfc243a5f43b55fbb17090
Sakar Masoud <masoudsakar@gmail.com>
2022-10-24 08:43:33 +0300

    Sakar Masoud

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const employee = {
+    name: "sakar",
+    streetAddress: "halabja",
+}
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmp = {...employee}
+    newEmp[key] = value
+    return newEmp
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = {...employee}
+    delete newEmp[key]
+    return newEmp
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
